f350e09c02d408718bf6176500a43f6fb41f1928
exauce-mwililikwa <exaucemwililikwa@gmail.com>
2021-05-19 01:13:57 +0200

    migration

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 	$db=mysqli_connect("localhost","root","","ecom_store");
 	function(){
-		global $db;ssSS
+		global $db;ssSSSSS
 	}mcmcmnxnxnxnxnnxnxsksk
 ?>ssssnsnsnsnsaanznznsNnssmssjsjsjsjsjsSSjssSSxkxkSSSxksskNSNSN
