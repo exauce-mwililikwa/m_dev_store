fbdc4c48da40c967fa1a1a0f5493a7a5b89aa934
exauce-mwililikwa <exaucemwililikwa@gmail.com>
2021-06-07 22:50:31 +0200

    migration

--- includes/db.php
+++ includes/db.php
@@ -1,4 +1,4 @@
 <?php
-//sssssssssssssssssssssssssssssssssssssssssssss	$con=mysqli_connect("localhost","root","","ecom_store");
+//sssssssssssssssssssssssssssssssssssssssssssssss	$con=mysqli_connect("localhost","root","","ecom_store");
 ?>
 
